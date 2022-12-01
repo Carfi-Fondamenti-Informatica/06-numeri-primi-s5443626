eb55f929fd5922831173f4d7201a63bef3777494
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-12-01 16:00:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include "lib.h"
 using namespace std;
 int main() {
-    int a;
+    int a=0;
     cin >> a;
     int b=(a-1);
-    if(numeroprimo (a,b)==true){
+    if(numeroprimo (a,b)){
         cout << "numero primo";
     } else if (numeroprimo(a,b)==false){
         cout << "numero non primo";
-    }
+    } return 0;
+}
