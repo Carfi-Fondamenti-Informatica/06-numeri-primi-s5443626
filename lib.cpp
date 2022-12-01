a03e5f185c74024da0f5b6e2feb9774e1f043ad7
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-12-01 15:52:41 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+//
+// Created by micma on 01/12/2022.
+//
+int numeroprimo(int a,int b){
+    bool x=true;
+    if (b!=1){
+        if ((a%b)!=0){
+            b--;
+            x= numeroprimo(a,b);
+        } else {
+            x=false;
+        }
+    }else{
+        x=true;
+    }return x;
+}
+#include "lib.h"
